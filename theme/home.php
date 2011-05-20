0e8d8d335da618d728c85f7aaa5ad63d40b701b5
Scott Meves <scott@stereointeractive.com>
2011-05-19 22:53:00 -0400

    adding home template

---
+++ theme/home.php
@@ -0,0 +1,60 @@
+<?php get_header(); ?>
+
+		<div class="main">
+      
+      <div class="sliderwrap">
+        <div class="slider">
+          <div class="slide" id="slide1">
+            <div class="grid_1 rollover top"><img src="images/photo1.png"></div>
+            <div class="grid_1 rollover top"><img src="images/photo2.png"></div>
+            <div class="grid_1 rollover top"><img src="images/photo3.png"></div>
+            <div class="grid_1 rollover top right"><img src="images/photo4.png"></div>
+            
+            <div class="grid_1 rollover bottom"><img src="images/photo5.png"></div>
+            <div class="grid_1 rollover bottom"><img src="images/photo6.png"></div>
+            <div class="grid_1 rollover bottom"><img src="images/photo7.png"></div>
+            <div class="grid_1 rollover bottom right"><img src="images/photo8.png"></div>
+          </div>
+          <div class="slide brochure" id="slide2">
+            
+            <div class="container">
+              <div class="grid_a alpha">
+                <h3><a href="#">retail leasing opportunity</a></h3>
+                <a href="#"><img src="images/555.jpg" style="margin-bottom: 28px"></a>
+
+                <h3><a href="#">proposed usage</a></h3>
+                <a href="#"><img src="images/usage.jpg"></a>
+              </div>
+              <div class="grid_b">
+                <h3><a href="about.html">about 555 california</a></h3>
+                <a href="about.html"><img src="images/exterior.jpg"></a>
+              </div>
+              <div class="grid_c omega">
+                <h3><a href="#">vicinity highlights</a></h3>
+                <a href="#"><img src="images/map.jpg" style="margin-bottom: 17px"></a>
+
+                <div class="col_half alpha">
+                  <h3><a href="#">virtual tour</a></h3>
+                  <a href="#"><img src="images/virtualtour.jpg"></a>
+                </div>
+                <div class="col_half omega">
+                  <h3><a href="#">sustainability</a></h3>
+                  <a href="#"><img src="images/sustainability.jpg"></a>
+                </div>
+              </div>
+            </div>
+            
+          </div>
+        </div>
+        <div class="edge transparent" id="arrow_left" style="display:none">
+          <a href="#" class="arrow prev">&lt;</a>
+        </div>
+        <div class="edge wide transparent" id="arrow_right" style="display:none">
+          <a href="#" class="explore next">EXPLORE</a> <a href="#" class="arrow next">&gt;</a>
+        </div>
+      </div>
+      
+      
+    </div>
+
+<?php get_footer(); ?>
